7d540d51d95cc77de502931ae0b92d10002e44d6
Dayana Joseph <dayanajosephofficial@gmail.com>
2024-04-17 11:04:30 -0400

    signupintergationdone

--- UI/src/pages/SignUp.js
+++ UI/src/pages/SignUp.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField, Box, Typography, Container } from '@mui/material';
+import axios from 'axios';
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the form from refreshing the page
+    try {
+   
+      const response = await axios.post('http://localhost:8011/signup', formData);
+      console.log('Signup successful:', response.data);
+      // Handle response and possibly redirect user or clear form
+    } catch (error) {
+      console.error('Error during signup:', error.response ? error.response.data : error.message);
+      // Handle errors, possibly setting an error message in state
+    }
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <Box
@@ -15,7 +43,7 @@ const SignUp = () => {
         <Typography component="h1" variant="h5">
           Sign Up
         </Typography>
-        <Box component="form" sx={{ mt: 1 }}>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
           <TextField
             margin="normal"
             required
@@ -25,6 +53,8 @@ const SignUp = () => {
             type="text"
             id="name"
             autoComplete="name"
+            value={formData.name}
+            onChange={handleChange}
           />
           <TextField
             margin="normal"
@@ -35,6 +65,8 @@ const SignUp = () => {
             type="email"
             id="email"
             autoComplete="email"
+            value={formData.email}
+            onChange={handleChange}
           />
           <TextField
             margin="normal"
@@ -45,6 +77,8 @@ const SignUp = () => {
             type="password"
             id="password"
             autoComplete="new-password"
+            value={formData.password}
+            onChange={handleChange}
           />
           <Button
             type="submit"
